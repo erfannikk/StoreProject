b4d6744ecbe9f77d2195039bdc75cf9c4db70968
Erfan Nikaan <erfan2004nikan@gmail.com>
2025-02-14 23:49:47 +0330

    updated

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,31 +1,14 @@
-import { Component } from '@angular/core';
-import {
-  FormControl,
-  FormGroupDirective,
-  NgForm,
-  Validators,
-  FormsModule,
-  ReactiveFormsModule,
-} from '@angular/forms';
-import { ErrorStateMatcher } from '@angular/material/core';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { NgIf } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    FormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    ReactiveFormsModule,
-    RouterLink
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
+  
 }
